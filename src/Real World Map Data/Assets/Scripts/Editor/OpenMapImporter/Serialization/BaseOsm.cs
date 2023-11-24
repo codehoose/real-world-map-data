c5f2a569c50cb45273adcef3d18619b6c6604045
Sloan Kelly <sloan@codehoose.com>
2023-11-24 12:13:10 -0500

    From a suggestion by @PavelTorgashov.

--- src/Real World Map Data/Assets/Scripts/Editor/OpenMapImporter/Serialization/BaseOsm.cs
+++ src/Real World Map Data/Assets/Scripts/Editor/OpenMapImporter/Serialization/BaseOsm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Xml;
+using UnityEngine;
 
 /*
     Copyright (c) 2017 Sloan Kelly
@@ -39,7 +40,30 @@ class BaseOsm
     {
         // TODO: We are going to assume 'attrName' exists in the collection
         string strValue = attributes[attrName].Value;
-        return (T)Convert.ChangeType(strValue, typeof(T));
+        T instance = default(T);
+
+        try
+        {
+            instance = (T)Convert.ChangeType(strValue, typeof(T));
+        }
+        catch (Exception e)
+        {
+            // Fix for height values ending with 'm' which seems to be something new.
+            if (strValue.EndsWith("m"))
+            {
+                var idx = strValue.IndexOf(' ');
+                if (idx >= 0)
+                {
+                    var tmp = strValue.Substring(0, idx);
+                    instance = (T)Convert.ChangeType(tmp, typeof(T));
+                }
+            }
+            else
+            {
+                Debug.Log(e.ToString() + "\r\nname=" + attrName + ", value=" + strValue);
+            }
+        }
+
+        return instance;
     }
 }
-
